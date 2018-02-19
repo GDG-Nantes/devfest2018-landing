f06bce3b6d849a9c4e8d0e0cef76bf0bc0a1bd28
Benjamin Petetot <bpetetot@gmail.com>
2018-02-19 16:23:26 +0100

    :tada: landing page DevFest 2018 (#1)

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+import Particles from "react-particles-js";
+import particlesConfig from "./particles";
+import logo from './logo-white.png'
+import rocket from './rocket.svg'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="landing-page">
+        <Particles params={particlesConfig} />
+        <div className="landing-content">
+          <img src={logo} alt="logo" width="40%"/>
+          <div className="landing-actions">
+            <a
+              href="https://drive.google.com/file/d/1zlMrEN71SfSDPuX4rRqAcLlB5BKjbAki/view?usp=sharing"
+              target="NEW"
+              className="btn btn-box"
+            >
+              <img src={rocket} width="80px" alt="download" />
+              <br/>
+              Télécharger le<br/>
+              dossier de Sponsoring
+            </a>
+          </div>
+          <div className="landing-footer">
+            <h2>Oct. 18-19 2018</h2>
+            <h2>Cité des Congrès, Nantes, France</h2>
+            <a href="mailto:bureau@gdgnantes.com">Contactez nous !</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
